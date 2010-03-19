c91ef0043017d1f1daca973b5f1634ba25a67bb3
Andrew Watson <andy@watsons.ch>
2010-03-19 22:49:26 +0100

    Example code actually does something.

--- example/example.c
+++ example/example.c
@@ -1,15 +1,22 @@
 #include "robopoly.h"
 #include "lcam.h"
 
+// Ce code de test fait clignoter la LED du PRisme (Pin C2)
+#define DELAY 200
+
 int main (void)
 {
 	lcam_initport();
 	lcam_setup();
+
 	while(1)
 	{
-		digitalWrite(A,2,1);
-		waitms(200);
-		digitalWrite(A,2,0);
-		waitms(200);
+		digitalWrite(C,2,1);
+		waitms(DELAY);
+		digitalWrite(C,2,0);
+		waitms(DELAY);
 	}
+
+	// never reached
+
 }
